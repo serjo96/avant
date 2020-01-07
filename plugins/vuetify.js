bbd28385a37053f85db43cc6feddc6984e50429e
serjo96 <king-beer@yandex.ru>
2020-01-08 01:12:41 +0500

    Added vuetify colors

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader version "^2.1.1" ,
+import colors from "vuetify/es5/util/colors";
 Vue.use(Vuetify)
 
 export default ctx => {
 	const vuetify = new Vuetify({
 		theme: {
-			dark: false // From 2.0 You have to select the theme dark or light here
+			dark: {
+				...colors
+			} // From 2.0 You have to select the theme dark or light here
 		}
 	})
 
