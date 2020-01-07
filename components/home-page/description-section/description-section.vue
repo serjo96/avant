a5cc9b36ed001238730c5e3ead65267936b65ed3
serjo96 <king-beer@yandex.ru>
2020-01-07 22:27:54 +0500

    Added description page

--- components/home-page/description-section/description-section.vue
+++ components/home-page/description-section/description-section.vue
@@ -1,6 +1,19 @@
 <template>
     <section class="description-section">
-
+        <div class="container">
+            <div class="description-section__content">
+                <div class="description">
+                    <h1 class="description__title">Hippocrates AI system </h1>
+                    <p class="description__text">Hippocrates AI system performs as a health care medical doctor advisor. Users may provide symptoms, ask medical questions and describe conditions in order to get diagnosis advise, including known medication and treatments. The system emulates MD.</p>
+                </div>
+                <div class="description-section__icon">
+                    <img src="~assets/heart.png" alt="">
+                </div>
+            </div>
+            <div class="description-section__button">
+                <nuxt-link :to="{ path: '/', hash:'#contact'}">CONTACT US NOW</nuxt-link>
+            </div>
+        </div>
     </section>
 </template>
 
