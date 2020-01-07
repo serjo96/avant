fc0f54322bdbcd8bd2b448f71e6e9320fce7fd06
serjo96 <king-beer@yandex.ru>
2020-01-07 22:27:34 +0500

    Change path to header

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-  import Header from '../components/header/header.vue';
+  import Header from '~/components/header/header.vue';
   import Footer from '~/components/footer/footer.vue';
 
   export default {
