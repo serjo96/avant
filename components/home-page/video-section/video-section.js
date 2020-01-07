98d3775916f7b14b046a07e24f9deb41e3fa8f5e
serjo96 <king-beer@yandex.ru>
2020-01-07 23:28:36 +0500

    Added video section

---
+++ components/home-page/video-section/video-section.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Component from 'nuxt-class-component';
+
+
+@Component({
+})
+class VideoSection extends Vue {
+
+
+}
+
+export default VideoSection;
---
+++ components/home-page/video-section/video-section.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="video-section">
+        <h3 class="section-title">This video is about our work</h3>
+    </section>
+</template>
+
+<script src="./video-section.js"></script>
+
+<style lang="stylus" src="./video-section.styl" scoped></style>
