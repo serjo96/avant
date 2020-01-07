287f787a087b47ffca67a28403bbba5be23b1c0d
serjo96 <king-beer@yandex.ru>
2020-01-07 23:21:50 +0500

    Added advantages section

---
+++ components/home-page/advantages/advantages.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Component from 'nuxt-class-component';
+
+
+@Component({
+})
+class Advantages extends Vue {
+
+
+}
+
+export default Advantages;
---
+++ components/home-page/advantages/advantages.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="advantages">
+        <div class="container">
+            <div class="advantages__container">
+                <div class="advantage">
+                    <div class="advantage__icon">
+                        <img src="~assets/aiguille.png" alt="">
+                    </div>
+                    <div class="advantage__text">
+                        <span class="chiffre-font">+70</span>
+                        <span class="text-font"> Specialized medical staff </span></div>
+                </div>
+                <div class="advantage">
+                    <div class="advantage__icon">
+                        <img src="~assets/pill.png" alt="">
+                    </div>
+                    <div class="advantage__text">
+                        <span class="chiffre-font">+20</span>
+                        <span class="text-font">Specialized medical staff</span>
+                    </div>
+                </div>
+
+                <div class="advantage">
+                    <div class="advantage__icon">
+                        <img src="~assets/sticker.png" alt="">
+                    </div>
+                    <div class="advantage__text">
+                        <span class="chiffre-font">+70</span>
+                        <span class="text-font"> Specialized medical staff</span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script src="./advantages.js"></script>
+<style lang="stylus" src="./advantages.styl" scoped></style>
