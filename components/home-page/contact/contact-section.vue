29b3ad4bc48a6400bcfe35f1ad6ad89028c40d01
serjo96 <king-beer@yandex.ru>
2020-01-09 03:07:54 +0500

    Added style for contact section

--- components/home-page/contact/contact-section.vue
+++ components/home-page/contact/contact-section.vue
@@ -5,31 +5,37 @@
                 <v-col cols="12" sm="5">
                     <div class="contact-form">
                         <h3 class="contact-title">Contact us</h3>
-                        <div class="contact-form__subtitle">we are available 24/7.</div>
+                        <div class="contact-form__subtitle">Use this form to get in touch with us, we are available 24/7.</div>
                         <v-form class="contact-form__form">
                             <v-text-field
                                     label="First Name *"
                                     outlined
+                                    color="light-blue darken-1"
                             ></v-text-field>
 
                             <v-text-field
                                     label="Last Name *"
                                     outlined
+                                    color="light-blue darken-1"
                             ></v-text-field>
 
                             <v-text-field
                                     label="Email *"
                                     outlined
+                                    color="light-blue darken-1"
                             ></v-text-field>
                             <v-textarea
                                     outlined
                                     name="input-7-4"
                                     label="message *"
+                                    color="light-blue darken-1"
                             ></v-textarea>
 
                             <v-btn
                                     outlined
                                     block
+                                    color="light-blue darken-1"
+                                    class="mt-2"
                             >Submit</v-btn>
                         </v-form>
 
@@ -40,7 +46,7 @@
                     <div class="contact-info">
                         <h3 class="contact-title">Important Info</h3>
                         <ul class="contact-list pa-0">
-                            <li class="contact-list__item">email: info@gopherprotocol.com</li>
+                            <li class="contact-list__item">Email: info@gopherprotocol.com</li>
                             <li class="contact-list__item">GBT Technologies.2500 Broadway, Suite F-125 Santa Monica, CA 90404 USA</li>
                         </ul>
 
