7572c8492ad336e7fa0ab0b5ba01f8ec63fb87dd
serjo96 <king-beer@yandex.ru>
2020-01-09 20:36:30 +0500

    Added info section

---
+++ components/home-page/info-section/info-section.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="description-section">
+        <div class="section-base">
+            <div class="container">
+                <div class="description-section__content">
+                    <div class="description">
+                        <h1 class="description__title">Hippocrates AI system </h1>
+                        <p class="description__text">The service is available for adults (18+) under a disclaimer that informs about the fact that the medical advices are AI, computer-based estimates and cannot replace a certified medical specialist.</p>
+                        <p class="description__text">The service is not intended to replace a “human” doctor’s medical advice but to provide a medical tips, based on known, public domain, medical information available on experts’ medical web sites, text books and similar.</p>
+                        <p class="description__text">The service is intended to provide medical advices and tips according to the latest medical standards and knowledge.</p>
+                    </div>
+                    <div class="description-section__icon">
+                        <img src="~assets/heart.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script src="./info-section.js"></script>
+<style src="./info-section.styl" lang="stylus" scoped></style>
