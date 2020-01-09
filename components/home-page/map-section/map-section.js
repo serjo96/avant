a07bda47261a8930e2b58572bf2a273dc71e877a
serjo96 <king-beer@yandex.ru>
2020-01-10 02:55:19 +0500

    Added map section

---
+++ components/home-page/map-section/map-section.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Component from 'nuxt-class-component';
+
+
+@Component({
+})
+class MapSection extends Vue {
+
+
+}
+
+export default MapSection;
---
+++ components/home-page/map-section/map-section.vue
@@ -0,0 +1,8 @@
+<template>
+    <section class="map-section">
+        <iframe src="https://snazzymaps.com/embed/210839" width="100%" height="450px" style="border:none;"></iframe>
+    </section>
+</template>
+
+<script src="./map-section.js"></script>
+<style lang="stylus" src="./map-section.styl" scoped></style>
