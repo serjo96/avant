0a6cc9b1e4c7fce8fca4915c14d79a4385435bf8
serjo96 <king-beer@yandex.ru>
2020-01-10 04:00:55 +0500

    Added style for social section

--- components/social/social.vue
+++ components/social/social.vue
@@ -5,32 +5,37 @@
                 <div class="col-md-6 col-sm-12 brdr">
                     <div class="social-follow">
                         <h4>follow us :</h4>
-                        <div class="nav">
-                            <ul>
-                                <li><a href="#"><v-icon>mdi-twitter</v-icon></a></li>
-                                <li><a href="#"><v-icon>mdi-dribbble</v-icon></a></li>
-                                <li><a href="#"><v-icon>mdi-facebook</v-icon></a></li>
-                                <li><a href="#"><v-icon>mdi-google</v-icon></a></li>
-                                <li><a href="#"><v-icon>mdi-youtube</v-icon></a></li>
-                                <li><a href="#"><v-icon>mdi-linkedin</v-icon></a></li>
-                                <li><a href="#"><v-icon>mdi-instagram</v-icon></a></li>
-                            </ul>
-                        </div>
+                        <ul class="social-area__nav">
+                            <li class="social-area__link"><a href="#"><v-icon color="white">mdi-twitter</v-icon></a></li>
+                            <li class="social-area__link"><a href="#"><v-icon color="white">mdi-dribbble</v-icon></a></li>
+                            <li class="social-area__link"><a href="#"><v-icon color="white">mdi-facebook</v-icon></a></li>
+                            <li class="social-area__link"><a href="#"><v-icon color="white">mdi-google</v-icon></a></li>
+                            <li class="social-area__link"><a href="#"><v-icon color="white">mdi-youtube</v-icon></a></li>
+                            <li class="social-area__link"><a href="#"><v-icon color="white">mdi-linkedin</v-icon></a></li>
+                            <li class="social-area__link"><a href="#"><v-icon color="white">mdi-instagram</v-icon></a></li>
+                        </ul>
                     </div>
                 </div>
                 <div class="col-md-6 col-sm-12">
                     <div class="subs-form-area">
-                        <h4>Subscribe : </h4>
-                        <form action="#" method="post">
+                        <h4 class="subs-form-area__title">Subscribe: </h4>
+                        <v-form
+                                action="#"
+                                class="subs-form-area__form"
+                                method="post"
+                        >
                             <v-text-field
                                 placeholder="Your Email"
                                 solo
+                                dense
                             ></v-text-field>
                             <v-btn
-                                    class="white--text"
+                                    class="ml-2 white--text"
                                     color="light-blue accent-2"
+                                    tile
+                                    normal
                             >Subscribe</v-btn>
-                        </form>
+                        </v-form>
                     </div>
                 </div>
             </div>
