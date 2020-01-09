6a13465c183630bce77edd05ac9baa19c3df7477
serjo96 <king-beer@yandex.ru>
2020-01-10 02:55:56 +0500

    Added new style for contact section

--- components/home-page/contact/contact-section.vue
+++ components/home-page/contact/contact-section.vue
@@ -1,61 +1,86 @@
 <template>
     <section class="contact-section">
         <div id="contact_form" class="container contact-container">
-            <v-row justify="space-between">
-                <v-col cols="12" sm="5">
-                    <div class="contact-form">
-                        <h3 class="contact-title">Contact us</h3>
-                        <div class="contact-form__subtitle">Use this form to get in touch with us, we are available 24/7.</div>
-                        <v-form class="contact-form__form">
-                            <v-text-field
-                                    label="First Name *"
-                                    outlined
-                                    color="light-blue darken-1"
-                            ></v-text-field>
-
-                            <v-text-field
-                                    label="Last Name *"
-                                    outlined
-                                    color="light-blue darken-1"
-                            ></v-text-field>
-
-                            <v-text-field
-                                    label="Email *"
-                                    outlined
-                                    color="light-blue darken-1"
-                            ></v-text-field>
-                            <v-textarea
-                                    outlined
-                                    name="input-7-4"
-                                    label="message *"
-                                    color="light-blue darken-1"
-                            ></v-textarea>
-
-                            <v-btn
-                                    outlined
-                                    block
-                                    color="light-blue darken-1"
-                                    class="mt-2"
-                            >Submit</v-btn>
-                        </v-form>
-
-                    </div>
-                </v-col>
-
-                <v-col cols="12" sm="5">
-                    <div class="contact-info">
-                        <h3 class="contact-title">Important Info</h3>
-                        <ul class="contact-list pa-0">
-                            <li class="contact-list__item">Email: info@gopherprotocol.com</li>
-                            <li class="contact-list__item">GBT Technologies.2500 Broadway, Suite F-125 Santa Monica, CA 90404 USA</li>
-                        </ul>
-
-                        <div class="contact-info__map">
-                            <iframe width="100%" height="420px" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=2500%20Broadway%2C%20Suite%20F-125%20Santa%20Monica%2090404%20USA+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=13&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">gps coordinates finder</a></iframe>
+            <div class="container">
+                <div class="row justify-center">
+                    <div class="col-md-6">
+                        <div class="contact-section__title text-center">
+                            <h2>Contact Us!</h2>
                         </div>
                     </div>
-                </v-col>
-            </v-row>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <div class="address-info light-blue accent-2">
+                            <div class="single-address">
+                                <div class="single-address__icon">
+                                    <v-icon color="grey lighten-5">mdi-email-outline</v-icon>
+                                </div>
+                                <div class="single-address__info">
+                                    info@gopherprotocol.com
+                                </div>
+                            </div>
+                            <div class="single-address">
+                                <div class="single-address__icon">
+                                    <v-icon color="grey lighten-5">mdi-map-marker-outline</v-icon>
+                                </div>
+                                <div class="single-address__info">
+                                    <p>GBT Technologies.2500 Broadway, Suite F-125 Santa Monica, CA 90404 USA</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="contact-area-form">
+                            <form action="#">
+                                <div class="row">
+                                    <div class="col-md-6 py-0">
+                                        <v-text-field
+                                                label="Your Name *"
+                                                solo
+                                                color="light-blue darken-1"
+                                        ></v-text-field>
+                                    </div>
+                                    <div class="col-md-6 py-0">
+                                        <v-text-field
+                                                label="Your Phone *"
+                                                solo
+                                                color="light-blue darken-1"
+                                        ></v-text-field>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12 py-0">
+                                        <v-text-field
+                                                label="Email *"
+                                                solo
+                                                color="light-blue darken-1"
+                                        ></v-text-field>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12 py-0">
+                                        <v-textarea
+                                                solo
+                                                name="input-7-4"
+                                                label="message *"
+                                                color="light-blue darken-1"
+                                        ></v-textarea>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12 py-0">
+                                        <v-btn
+                                                class="white--text"
+                                                color="light-blue accent-2"
+                                        >Submit</v-btn>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
