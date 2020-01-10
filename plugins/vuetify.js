e9f38cc6f0bc446cce6fb2134bab7eba30736f7d
serjo96 <king-beer@yandex.ru>
2020-01-10 21:46:29 +0500

    Added material design

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader version "^2.1.1" ,
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import colors from "vuetify/es5/util/colors";
 Vue.use(Vuetify)
 
@@ -9,6 +10,7 @@ export default ctx => {
 	const vuetify = new Vuetify({
 		theme: {
 			dark: {
+				background: colors.shades.white,
 				...colors
 			} // From 2.0 You have to select the theme dark or light here
 		}
