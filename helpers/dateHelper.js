9b7ce06904620aae168dc7e53db8b2484c7f5251
serjo96 <king-beer@yandex.ru>
2020-01-14 02:30:33 +0500

    Updated date helper

--- helpers/dateHelper.js
+++ helpers/dateHelper.js
@@ -1,1 +1,9 @@
-export const dateHelper = (date) => `${new Date(date).getHours()}:${new Date(date).getMinutes()}`
+export const dateHelper = (oldDate) => {
+	const date = new Date(oldDate);
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+
+	if (hours   < 10) {hours   = "0"+hours;}
+	if (minutes < 10) {minutes = "0"+minutes;}
+	return hours + ':' + minutes;
+};
