65f29c0da5b4706770fef322067caadfcbecf09d
serjo96 <king-beer@yandex.ru>
2020-01-14 17:20:18 +0500

    Changed theme primary color

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,13 +4,14 @@ import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader version "^2.1.1" ,
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import colors from "vuetify/es5/util/colors";
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 
 export default ctx => {
 	const vuetify = new Vuetify({
 		theme: {
 			dark: {
 				background: colors.shades.white,
+				primary: colors.lightBlue.accent2,
 				...colors
 			} // From 2.0 You have to select the theme dark or light here
 		}
