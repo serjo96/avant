ae24a5569474103be124ad49b252c603cccbdbd7
serjo96 <king-beer@yandex.ru>
2020-01-18 04:07:31 +0500

    Added step-section

---
+++ components/home-page/steps-section/steps-section.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+
+
+@Component({
+})
+class StepSection extends Vue {
+
+}
+
+export default StepSection;
---
+++ components/home-page/steps-section/steps-section.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="steps-section">
+        <Container>
+            <div class="step-list row">
+                <div class="col-12 col-md-4 col-sm-3">
+                    <div class="step">
+                        <div class="step__number">01</div>
+                        <div class="step__title">Step 1</div>
+                        <div class="step__content">
+                            <p class="step__text">Add short description of the Step 1, so your customers could feel how it's easy to get a solution with your product, service or event</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-4 col-sm-3">
+                    <div class="step">
+                        <div class="step__number">02</div>
+                        <div class="step__title">Step 2</div>
+                        <div class="step__content">
+                            <p class="step__text">Add short description of the Step 1, so your customers could feel how it's easy to get a solution with your product, service or event</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-4 col-sm-3">
+                    <div class="step">
+                        <div class="step__number">03</div>
+                        <div class="step__title">Step 3</div>
+                        <div class="step__content">
+                            <p class="step__text">Add short description of the Step 1, so your customers could feel how it's easy to get a solution with your product, service or event</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </Container>
+    </section>
+</template>
+
+<script src="./steps-section.js"></script>
+<style lang="stylus" src="./steps-section.styl" scoped></style>
