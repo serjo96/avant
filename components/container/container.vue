67b33bd935dd8b597e78f5afb728995e5f17482d
serjo96 <king-beer@yandex.ru>
2020-01-18 04:07:17 +0500

    Added  container component

---
+++ components/container/container.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container my-container>
+        <slot></slot>
+    </v-container>
+</template>
+
+<script>
+	export default {
+		name: "Container"
+	}
+</script>
+
+<style lang="stylus" src="./container.styl" scoped></style>
