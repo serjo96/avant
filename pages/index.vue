03336b8ec8f6cb630252bd23f39ba7c276e85087
serjo96 <king-beer@yandex.ru>
2020-01-18 04:10:39 +0500

    Added Step section to the main page

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <DescriptionSection />
     <Advantages />
     <AboutSection />
+    <StepSection></StepSection>
 <!--    <VideoSection />-->
 
 <!--    <InfoSection />-->
@@ -13,7 +14,7 @@
 </template>
 
 <script>
-
+import {Fragment} from "vue-fragment";
 import DescriptionSection from "~/components/home-page/description-section/description-section.vue";
 import Advantages from "~/components/home-page/advantages/advantages.vue";
 import VideoSection from "~/components/home-page/video-section/video-section.vue";
@@ -22,10 +23,11 @@ import InfoSection from "~/components/home-page/info-section/info-section.vue";
 import MapSection from "~/components/home-page/map-section/map-section.vue";
 import Social from "~/components/social/social.vue";
 import AboutSection from "~/components/home-page/about-section/about-section.vue";
-import {Fragment} from "vue-fragment";
+import StepSection from "~/components/home-page/steps-section/steps-section.vue";
 
 export default {
   components: {
+    Fragment,
     DescriptionSection,
     Advantages,
     VideoSection,
@@ -34,7 +36,7 @@ export default {
     MapSection,
     Social,
     AboutSection,
-    Fragment
+    StepSection
   }
 }
 </script>
