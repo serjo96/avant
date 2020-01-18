1759736919e5e607021d793d934e156e9089c321
serjo96 <king-beer@yandex.ru>
2020-01-19 04:33:54 +0500

    Added set user mutation

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -4,5 +4,5 @@ export default function ({ isServer, store, req }) {
 	// If nuxt generate, pass this middleware
 	if (process.server && !req) return
 	const loggedUser = process.server ? getUserFromCookie(req) : getUserFromLocalStorage()
-	store.commit('SET_USER', loggedUser)
+	store.commit('authorization/SET_USER', loggedUser)
 }
