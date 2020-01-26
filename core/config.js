de0fccc918aa0d0e972ef6ef2c642de3b395c90a
serjo96 <king-beer@yandex.ru>
2020-01-27 04:54:32 +0500

    Updated config file

--- core/config.js
+++ core/config.js
@@ -1,1 +1,1 @@
-export const API_PATH = process.env.NODE_ENV !== 'development' ? 'https://playshow.online' : 'http://localhost:3000';
+export const API_PATH = process.env.NODE_ENV !== 'development' ? 'http://50.18.214.237:3000' : 'http://localhost:3000';
