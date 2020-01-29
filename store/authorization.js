17fc865156e8186c17edb9931efbc597897c099a
serjo96 <king-beer@yandex.ru>
2020-01-29 21:46:19 +0500

    Added response message store

--- store/authorization.js
+++ store/authorization.js
@@ -1,119 +1,31 @@
-import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
-import axios from 'axios';
-import { API_PATH } from "~/core/config";
+import { Module, VuexModule, Mutation } from 'vuex-module-decorators';
 
 @Module({
 	stateFactory: true,
-	namespaced: true,
-	name: 'Authorization'
 })
 class Authorization extends VuexModule {
-	__auth = null;
-	__router = null;
-	user = {};
-	authError = {
-		code: '',
-		message: ''
+	responseMessage = {
+		message: '',
+		status: false,
+		confirm: false
 	};
-	successResetPasswordMessage = '';
 
-	get AuthError() {
-		return this.authError;
-	}
-
-	get loggedUser() {
-		return this.user
-	}
-
-	get isAuthenticated() {
-		return !!this.user
-	}
-
-
-	// @Action({rawError: true})
-	// async register() {
-	// 	try {
-	// 		await this.$axios.post('register', {
-	// 			username: this.username,
-	// 			email: this.email,
-	// 			password: this.password
-	// 		})
-	//
-	// 		await this.$auth.loginWith('local', {
-	// 			data: {
-	// 				email: this.email,
-	// 				password: this.password
-	// 			},
-	// 		})
-	//
-	// 		this.$router.push('/')
-	// 	} catch (e) {
-	// 		this.error = e.response.data.message
-	// 	}
-	// }
 
 	@Mutation
-	SET_USER(user) {
-		console.log(user);
-		this.user = user ;
-	}
-
-	@Mutation
-	error(error) {
-		this.authError = error;
-	}
-
-	@Mutation
-	successResetPassword() {
-		this.successResetPasswordMessage = 'A message with instructions for resetting the password has been sent to your email.';
-	}
-
-	@Mutation
-	login(payload) {
-		this.user = payload.result;
-	}
-
-	@Action
-	logOut() {
-		// firebase.auth().signOut()
-		// 	.then((res)=> {
-		// 		console.log(res);
-		// 		this.context.commit('setCurrentUser', firebase.auth().currentUser);
-		// 		localStorage.removeItem('user');
-		// 		Router.push('/auth');
-		// 	})
-		// 	.catch(error=> {
-		// 		console.log(error);
-		// 		this.context.commit('setCurrentUser', firebase.auth().currentUser);
-		// 	});
-	}
-
-	@Action
-	resetPassword(email) {
-		// const actionCodeSettings = {url: 'http://localhost:8080'};
-		//
-		// firebase.auth().sendPasswordResetEmail(email, actionCodeSettings)
-		// 	.then(()=> {
-		// 		this.context.commit('successResetPassword');
-		// 	})
-		// 	.catch(error=> {
-		// 		this.context.commit('error', error);
-		// 	});
+	setResponseMessage(payload) {
+		this.responseMessage = payload;
 	}
 
 
 	@Mutation
-	clearErrorData() {
-		this.authError = {
-			code: '',
-			message: ''
+	clearResponseData() {
+		this.responseMessage = {
+			message: '',
+			status: false,
+			confirm: false
 		};
 	}
 
-	@Mutation
-	clearPasswordMessage() {
-		this.successResetPasswordMessage = '';
-	}
 }
 
 export default Authorization;
