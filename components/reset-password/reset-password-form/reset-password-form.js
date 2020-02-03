69ec491861a5655ba39d44ad5df0a0be123ee06d
serjo96 <king-beer@yandex.ru>
2020-02-04 03:40:08 +0500

    Added reset password form

---
+++ components/reset-password/reset-password-form/reset-password-form.js
@@ -0,0 +1,65 @@
+import Vue from 'vue';
+import Component from 'nuxt-class-component';
+
+
+@Component({})
+class ResetPasswordForm extends Vue {
+	valid = true;
+	passwordRules = {
+		required: value => !!value || 'Required.',
+		min: v => v.length >= 6 || 'Min 6 characters',
+	};
+	progressText() {
+		const password = this.password;
+		let prog = [/[$@$!%*#?&]/, /[A-Z]/, /[0-9]/, /[a-z]/]
+			.reduce((memo, test) => memo + Number(test.test(password)), 0);
+
+		if(prog > 2 && password.length >= 6){
+			prog++;
+		}
+
+		let progress = 0;
+		switch (prog) {
+			case 0:
+			case 1:
+				progress = 0;
+				break;
+			case 2:
+				progress = 1;
+				break;
+			case 3:
+				progress = 2;
+				break;
+			case 4:
+				progress = 3;
+				break;
+			case 5:
+				progress = 4;
+				break;
+		}
+
+		return progress;
+	}
+
+	get passwordStatus(){
+		const passwordStatus  = {
+			0: 'Very weak',
+			1: 'Weak',
+			2: 'Average',
+			3: 'Strong',
+			4: 'Very strong',
+		};
+
+		return passwordStatus[this.progressText()]
+	}
+
+	progress () {
+		return Math.min(100, this.progressText() * 25)
+	}
+
+	color () {
+		return ['darken-4 red', 'error', 'darken-4 lime', 'warning', 'success'][this.progressText()]
+	}
+}
+
+export default ResetPasswordForm;
