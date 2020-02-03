c072c79d249c77094b9fa34ff7f593378643a44c
serjo96 <king-beer@yandex.ru>
2020-02-04 03:41:10 +0500

    Added component state for reset password

--- pages/auth/reset-password/index.vue
+++ pages/auth/reset-password/index.vue
@@ -3,60 +3,9 @@
         <div class="reset-password">
             <div class="reset-password__title">Reset password</div>
 
-            <v-form
-                    ref="form"
-                    v-model="valid"
-                    @keyup.native.enter="onSubmit"
-            >
-                <v-text-field
-                        :rules="emailRules"
-                        v-model="email"
-                        label="E-mail"
-                        type="email"
-                        required
-                        color="blue-grey lighten-4"
-                        prepend-inner-icon="email"
-                        solo
-                        dark
-                ></v-text-field>
-
-
-                <v-btn
-                        block
-                        @click="onSubmit"
-                        color="blue-grey lighten-5"
-                        class="login__button mb-3">
-                    Reset password
-                </v-btn>
-            </v-form>
-
-            <v-btn
-                    class="reset-password__buck-navigate white--text"
-                    color="light-blue darken-1"
-                    nuxt
-                    to="sign-in"
-                   >
-                <v-icon left dark>arrow_back_ios</v-icon>
-                Buck to login
-            </v-btn>
-
-            <div class="response-message">
-                <div
-                        :class="{
-                        'response-message__text' : true,
-                        'red darken-2': !responseMessage.status
-                       }"
-                >
-                    {{responseMessage.message}}
-                </div>
-                <button
-                        v-if="responseMessage.confirm"
-                        class="response-message__resend-confirm"
-                        @click="resentConfirm"
-                >
-                    Resend confirm message?
-                </button>
-            </div>
+            <component
+                    :is="resetPasswordComponent"
+            ></component>
         </div>
     </div>
 </template>
--- pages/auth/reset-password/resetPassword.js
+++ pages/auth/reset-password/resetPassword.js
@@ -1,42 +1,17 @@
 import Vue from 'vue';
-import Component  from 'nuxt-class-component';
-import { Prop } from 'vue-property-decorator'
-import { Action, Mutation, Getter } from 'vuex-class';
+import Component, { State } from 'nuxt-class-component';
+import ResetPasswordEmail from "~/components/reset-password/reset-password-email/reset-password-email.vue";
+import ResetPasswordForm from "~/components/reset-password/reset-password-form/reset-password-form.vue";
 
 
 
 @Component({
-    data: ()=> ({
-        emailRules: [
-            v => !!v || 'E-mail is required',
-            v => /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(v) || 'E-mail must be valid'
-        ],
-
-    }),
+    components: { ResetPasswordEmail, ResetPasswordForm },
     middleware: 'resetPassword'
 })
 class ResetPassword extends Vue {
-    email = '';
-    valid = true;
-    @Prop(Function) changeComponent;
-    @Prop() responseMessage;
-    @Mutation('authorization/setResponseMessage') setResponseMessage;
+    @State(state => state.authorization.resetPasswordComponent)  resetPasswordComponent;
 
-    get formValidate(){
-        return this.$refs.form .validate();
-    }
-
-    async onSubmit(){
-        if(this.formValidate){
-            try {
-                const res = await this.$axios.get(`/auth/email/forgot-password/${this.email}`);
-                this.setResponseMessage(res);
-                this.$router.push('/auth/sign-in');
-            } catch ({response: {data}}) {
-                this.setResponseMessage(data.error);
-            }
-        }
-    }
 
 }
 
