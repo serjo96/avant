d92fd1e084f12a2152abba9103ac9d4ea7dd5c66
serjo96 <king-beer@yandex.ru>
2020-02-04 03:04:51 +0500

    Added store global module

---
+++ store/global.js
@@ -0,0 +1,12 @@
+import { Module, VuexModule, Mutation } from 'vuex-module-decorators';
+
+
+@Module({
+	stateFactory: true,
+})
+class Authorization extends VuexModule {
+
+
+}
+
+export default Authorization;
