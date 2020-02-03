102369e349c1db03e44a2370335db8b719e3b592
serjo96 <king-beer@yandex.ru>
2020-02-04 03:05:06 +0500

    Added global store

--- store/index.js
+++ store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex, { Module } from 'vuex';
 import Authorization from './authorization';
+import global from './global';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
 	modules: {
 		Authorization,
+		global,
 	}
 });
