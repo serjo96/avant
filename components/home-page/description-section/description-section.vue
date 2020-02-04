ee62c6b9d79a93f56754d07abf5884915134ebde
serjo96 <king-beer@yandex.ru>
2020-02-04 15:35:47 +0500

    Added text for discr section

--- components/home-page/description-section/description-section.vue
+++ components/home-page/description-section/description-section.vue
@@ -4,7 +4,9 @@
             <div class="description-section__content">
                 <div class="description">
                     <h1 class="description__title">Hippocrates AI system </h1>
-                    <p class="description__text">Hippocrates AI system performs as a health care medical doctor advisor. Users may provide symptoms, ask medical questions and describe conditions in order to get diagnosis advise, including known medication and treatments. The system emulates MD.</p>
+                    <p class="description__text">
+                        Hippocrates health system is an AI based medical advisor. Users may provide symptoms, ask medical questions and describe conditions in order to get diagnosis advise, including known medication and treatments. This means a reliable, simple and fast medical assistance experiences for people around the world. We provide essential capabilities necessary to advice patients about medical symptoms and conditions based on data and analytics taken from The National Center for Biotechnology, CDC and other accredited resources. The service is available for adults (18+) under a disclaimer and is not intend to replace a human physician.
+                    </p>
                     <div class="description-section__button">
                         <v-btn
                                 nuxt
