fd09123812cb1751edfeb52bcb610088044d64d3
serjo96 <king-beer@yandex.ru>
2020-02-10 20:28:31 +0500

    Added chat container style

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="chat-page">
-        <section class="module">
+        <section class="chat-page__container">
             <div class="container">
+                <div class="chat-page__header">
+                    <h3>Welcome to Hippocrates AI-based medical advisor!</h3>
+                    <h3>For any feedback please email <a  href="mailto:info@gopherprotocol.com">info@gopherprotocol.com</a></h3>
+                </div>
                 <Chat />
 <!--                <object type="text/html" id="bot" data="https://landbot.io/u/H-25531-0WYHN5OX5DBFTPIG/index.html" width="100%" style="height: 938px;"></object>-->
             </div>
@@ -21,10 +25,25 @@
 </script>
 
 <style lang="stylus" scoped>
-    .chat-page{
+    .chat-page {
         background: #fff
-        padding: 30px 0
+        width: 100%;
         height: 100%
+        padding: 30px 0
+        &__container {
+            margin: 0 auto
+            @media screen and (min-width: 425px) {
+                width: 80%;
+                max-width: 700px;
+            }
+            @media screen and (min-width: 768px) {
+                width: 80%;
+            }
+
+            @media screen and (min-width: 1024px) {
+                width: 60%;
+            }
+        }
     }
 
     .landbot__bot {
