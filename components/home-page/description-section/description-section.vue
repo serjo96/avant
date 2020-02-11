5a250046726e677c058ddc643fa0f0424a15a00e
serjo96 <king-beer@yandex.ru>
2020-02-12 03:02:14 +0500

    Changed background for that section

--- components/home-page/description-section/description-section.vue
+++ components/home-page/description-section/description-section.vue
@@ -1,29 +1,32 @@
 <template>
     <section class="description-section" id="description-section">
-        <Container>
-            <div class="description-section__content">
-                <div class="description">
-                    <h1 class="description__title">Hippocrates - AI-based medical advisor</h1>
-                    <p class="description__text">
-                        Hippocrates health system is an AI based medical advisor. Users may provide symptoms, ask medical questions and describe conditions in order to get diagnosis advise, including known medication and treatments. This means a reliable, simple and fast medical assistance experiences for people around the world. We provide essential capabilities necessary to advice patients about medical symptoms and conditions based on data and analytics taken from The National Center for Biotechnology, CDC and other accredited resources. The service is available for adults (18+) under a disclaimer and is not intend to replace a human physician.
-                    </p>
-                    <div class="description-section__button">
-                        <v-btn
-                                nuxt
-                                large
-                                color="white"
-                                class="font-weight-light"
-                                to="chat"
-                        >
-                            get started now
-                        </v-btn>
+        <div class="description-section__base">
+            <Container>
+                <div class="description-section__content">
+                    <div class="description">
+                        <h1 class="description__title">Hippocrates - AI-based medical advisor</h1>
+                        <p class="description__text">
+                            Hippocrates health system is an AI based medical advisor. Users may provide symptoms, ask medical questions and describe conditions in order to get diagnosis advise, including known medication and treatments. This means a reliable, simple and fast medical assistance experiences for people around the world. We provide essential capabilities necessary to advice patients about medical symptoms and conditions based on data and analytics taken from The National Center for Biotechnology, CDC and other accredited resources. The service is available for adults (18+) under a disclaimer and is not intend to replace a human physician.
+                        </p>
+                        <div class="description-section__button">
+                            <v-btn
+                                    nuxt
+                                    large
+                                    color="white"
+                                    class="font-weight-light"
+                                    to="chat"
+                            >
+                                get started now
+                            </v-btn>
+                        </div>
+                    </div>
+                    <div class="description-section__icon">
+                        <img src="~assets/heart.png" alt="">
                     </div>
                 </div>
-                <div class="description-section__icon">
-                    <img src="~assets/heart.png" alt="">
-                </div>
-            </div>
-        </Container>
+            </Container>
+
+        </div>
     </section>
 </template>
 
