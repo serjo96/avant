64ec4b19b6c1c45daeaf24d355f53903f92e82f1
serjo96 <king-beer@yandex.ru>
2020-02-12 03:03:48 +0500

    Removed material design font

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:200,400,700,300,600&display=swap', crossOrigin: 'anonymous' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons', crossOrigin: 'anonymous'}
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700', crossOrigin: 'anonymous'}
     ]
   },
   buildModules: [
