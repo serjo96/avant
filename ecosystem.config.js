fd2732cca7ef5f04ab21f1f2903e8bead05f635e
serjo96 <king-beer@yandex.ru>
2020-02-13 15:37:13 +0500

    Updated ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,8 @@ module.exports = {
 		{
 			name: "nuxt-prod",
 			instances: 1,
-			autorestart: true,
-			watch: true,
-			ignore_watch : ["node_modules"],
-			max_memory_restart: '1G',
 			script: "npm",
-			args: "run start",
+			args: "run dev",
 		}
 	]
 };
