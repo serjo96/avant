9d93c4ebd67547c94550beb749c813b77ae2c4b9
serjo96 <king-beer@yandex.ru>
2020-02-18 16:20:37 +0500

    Added avatar uploader

--- components/profile-settings/profile-settings.vue
+++ components/profile-settings/profile-settings.vue
@@ -70,21 +70,30 @@
                     </div>
                 </div>
                 <div class="profile-avatar">
-                    <v-avatar
-                            color="primary white--text"
-                            size="128"
-                            class="profile-avatar__image"
+                    <input accept="image/*"
+                           id="upload-image"
+                           class="profile-avatar__uploader"
+                           type="file"
+                           ref="uploaderInput"
+                           @change="uploadFile"
                     >
-                        <img
-                                v-if="value.photos.profilePic.src"
-                                :src="value.photos.profilePic.src"
-                                alt="profile"
+                    <label for="upload-image">
+                        <v-avatar
+                                color="primary white--text"
+                                size="128"
+                                class="profile-avatar__image"
                         >
-                        <div v-if="!value.photos.profilePic.src">{{value.name ? value.name[0] : value.email[0]}}</div>
-                        <div class="profile-avatar__change-btn">
-                            <span>Edit avatar</span>
-                        </div>
-                    </v-avatar>
+                                <img
+                                        v-if="value.profilepicture"
+                                        :src="value.profilepicture"
+                                        alt="profile"
+                                >
+                                <div v-if="!value.profilepicture">{{value.name ? value.name[0] : value.email[0]}}</div>
+                                <div class="profile-avatar__change-btn">
+                                    <span>Edit avatar</span>
+                                </div>
+                        </v-avatar>
+                    </label>
                 </div>
             </div>
         </div>
