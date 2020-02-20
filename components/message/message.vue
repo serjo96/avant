51b5c3b0336ad5e7ae89063ff60eeab51fe4236c
serjo96 <king-beer@yandex.ru>
2020-02-21 00:40:10 +0500

    Added message collapse

--- components/message/message.vue
+++ components/message/message.vue
@@ -21,23 +21,51 @@
                     <div class="message__container">
                         <h3 class="message__title" v-if="messageData.title">{{messageData.title}}</h3>
 
-                        <p v-if="messageData.message"
-                           class="message__text"
-                           v-html="$md.render(messageData.message)"
-                        ></p>
 
-                        <div v-if="messageData.description" class="message__content message__content--description">
+                            <p v-if="messageData.message"
+                               class="message__text"
+                               v-html="$md.render(messageData.message)"
+                            ></p>
+
+
+
+                        <div v-if="messageData.description" class="message__content message__content--description collapse">
                             <h3>Description</h3>
-                            <p v-html="$md.render(messageData.description)"></p>
+                            <input class="collapse__input" type="checkbox" id="chck1">
+                            <div class="collapse__content">
+                                <p v-html="$md.render(messageData.description)"></p>
+                                <div class="collapse__shadow"></div>
+                            </div>
+
+                            <v-btn color="primary" class="px-0" outlined x-small>
+                                <label class="collapse__label" for="chck1">Show more </label>
+                            </v-btn>
+
+
                         </div>
 
-                        <div v-if="messageData.treatments" class="message__content message__content--treatments">
+                        <div v-if="messageData.treatments" class="message__content message__content--treatments collapse">
                             <h3>Treatments</h3>
-                            <p v-html="$md.render(messageData.treatments)"></p>
+                            <input class="collapse__input" type="checkbox" id="treatments">
+                            <div class="collapse__content">
+                                <p v-html="$md.render(messageData.treatments)"></p>
+                                <div class="collapse__shadow"></div>
+                            </div>
+                            <v-btn color="primary" class="px-0" outlined x-small>
+                                <label class="collapse__label" for="treatments">Show more </label>
+                            </v-btn>
                         </div>
-                        <div v-if="messageData.prevention" class="message__content message__content--prevention">
+                        <div v-if="messageData.prevention" class="message__content message__content--prevention collapse">
                             <h3>Prevention</h3>
-                            <p v-html="$md.render(messageData.prevention)"></p>
+
+                            <input class="collapse__input" type="checkbox" id="prevention">
+                            <div class="collapse__content">
+                                <p v-html="$md.render(messageData.prevention)"></p>
+                                <div class="collapse__shadow"></div>
+                            </div>
+                            <v-btn color="primary" class="px-0" outlined x-small>
+                                <label class="collapse__label" for="prevention">Show more </label>
+                            </v-btn>
                         </div>
                         <a
                                 v-if="messageData.publications"
