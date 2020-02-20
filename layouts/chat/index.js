ea50bf0fc19199c66d058cad743689e48f76a6a4
serjo96 <king-beer@yandex.ru>
2020-02-20 17:55:51 +0500

    Added profile component in chat layout

--- layouts/chat/index.js
+++ layouts/chat/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Component, {Mutation, State} from 'nuxt-class-component';
 import Header from '~/components/header/header.vue';
+import Profile from "~/components/profile/profile.vue";
 import "vuetify/src/components/VGrid/VGrid.sass";
 import "vuetify/src/components/VGrid/_grid.sass";
 import '~/assets/styles/variables/vuetify.scss'
@@ -8,24 +9,15 @@ import '~/assets/styles/variables/vuetify.scss'
 @Component({
 	components: {
 		Header,
+		Profile
 	}
 })
 class ChatLayout extends Vue {
 	showProfileSettings = false;
 	isAuthenticated = this.$auth.loggedIn;
-	@State(state => state.authorization.user) user;
-	@Mutation('authorization/setUser') setUser;
 
-	async onShowProfileSettings() {
-		try {
-			const {data: {data}} = await this.$axios.get(`/users/user/${this.user.email}`);
-
-			setTimeout(()=> {this.setUser(data.data)}, 0);
-			this.showProfileSettings = true;
-		} catch (err) {
-			console.warn(err)
-		}
-
+	onShowProfileSettings() {
+		this.showProfileSettings = true;
 	}
 
 }
