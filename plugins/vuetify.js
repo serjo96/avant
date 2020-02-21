cfffa891d7a54d3dbe736d3495317b235ec8580e
serjo96 <king-beer@yandex.ru>
2020-02-21 15:15:49 +0500

    Removed vuetify import

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import colors from "vuetify/es5/util/colors";
 import "vuetify/src/components/VGrid/VGrid.sass";
@@ -10,8 +9,7 @@ Vue.use(Vuetify);
 export default ctx => {
 	const vuetify = new Vuetify({
 		theme: {
-			dark: {
-				background: colors.shades.white,
+			light: {
 				primary: colors.lightBlue.accent2,
 				...colors
 			} // From 2.0 You have to select the theme dark or light here
