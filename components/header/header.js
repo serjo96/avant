0a717d894c2a868b7ffbd100b48991fbb4cea996
serjo96 <king-beer@yandex.ru>
2020-02-22 04:19:56 +0500

    Added fix scroll when menu is open

--- components/header/header.js
+++ components/header/header.js
@@ -20,6 +20,7 @@ class Header extends Vue {
 	toggleNav() {
 		if (window.innerWidth <= 960) {
 			this.mobileNav = !this.mobileNav;
+			document.documentElement.classList.toggle('overflow-y-hidden')
 		}
 	}
 
