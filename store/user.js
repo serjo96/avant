6f3de0dbe3f1f21ab0c3ce3e2459134e6b9235ae
serjo96 <king-beer@yandex.ru>
2020-02-29 02:22:22 +0500

    Added init user data

--- store/user.js
+++ store/user.js
@@ -2,21 +2,22 @@ import { Module, VuexModule, Mutation } from 'vuex-module-decorators';
 import { getUserFromLocalStorage, setUser } from "~/utils/auth";
 import { API_PATH } from "~/core/config";
 
-const localUser = process.browser ? JSON.parse(getUserFromLocalStorage()) : {
-	userID: '',
-	name: '',
-	email: '',
-	password: '',
-	sex: '',
-	birthdaydate: '',
-	avatar: '',
-};
+const localUser = process.browser ? getUserFromLocalStorage() : null;
+
 
 @Module({
 	stateFactory: true,
 })
 class User extends VuexModule {
-	userData = localUser;
+	userData = localUser ? JSON.parse(localUser) : {
+		userID: '',
+		name: '',
+		email: '',
+		password: '',
+		sex: '',
+		birthdaydate: '',
+		avatar: '',
+	};
 
 	get userFormatted() {
 		const birthdaydate = this.userData.birthdaydate ? new Date(this.userData.birthdaydate).toISOString().substr(0, 10) : '';
