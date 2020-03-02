f5dee930dd22c7eb54c8335b60954ae9f9bc263c
serjo96 <king-beer@yandex.ru>
2020-03-03 02:32:46 +0500

    Added axios

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,9 +1,13 @@
 import {getTokenFromLocalStorage, getUserFromLocalStorage} from "../utils/auth";
+import axios from 'axios';
+import {API_PATH} from "../core/config";
 
-export default async function ({$axios, store}) {
+export default async function ({ store}) {
 	const token = getTokenFromLocalStorage();
 	if( token ) {
-		$axios.setHeader('Authorization', token);
+		// $axios.setHeader('Authorization', token);
+		axios.defaults.baseURL = API_PATH;
+		axios.defaults.headers.common['Authorization'] = token;
 	}
 
 	if (getUserFromLocalStorage() ) {
