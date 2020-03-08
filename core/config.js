c863953dd2e7702b9bc2d3434905e08449e5070d
serjo96 <king-beer@yandex.ru>
2020-03-09 01:11:24 +0500

    Fixed api path

--- core/config.js
+++ core/config.js
@@ -1,1 +1,1 @@
-export const API_PATH = process.env.NODE_ENV !== 'development' ? 'https://www.hmd.care/api' : 'http://localhost:3000';
+export const API_PATH = process.env.NODE_ENV !== 'development' ? '/api' : 'http://localhost:3000';
